049a2ff378bcf2313a622c2a0abbf19cd904e4f2
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2022-03-27 15:17:37 +0300

    Upload project

---
+++ router.js
@@ -0,0 +1,41 @@
+const routes = {
+  home: "home-template",
+  login: "login-form-template",
+  register: "register-form-template",
+  "add-movie": "add-movie-template",
+  details: "details-template",
+};
+
+const router = async (fullPath) => {
+  let [path, id] = fullPath.split("/");
+  console.log(`${id} is id of path - ${path}`);
+  let app = document.getElementById("app");
+  let templateData = authServices.getData();
+
+  switch (path) {
+    case "home":
+      templateData.movies = await movieSurvices.getAll();
+      break;
+    case "logout":
+      authServices.logout();
+      return navigate("home");
+      break;
+    case "details":
+        let movieDetails = await movieSurvices.getOne(id);
+        Object.assign(templateData, movieDetails);
+        console.log(templateData)
+        console.log(movieDetails)
+        break;
+        case "delete":
+       await movieSurvices.deleteOne(id);
+      break;
+    default:
+      break;
+  }
+
+  let temp = Handlebars.compile(
+    document.getElementById(routes[path]).innerHTML
+  );
+
+  app.innerHTML = temp(templateData);
+};
